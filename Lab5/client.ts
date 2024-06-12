dece00c7cf14e0c12c879ecf19e79d3063c06bea
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-12 03:45:42 -0400

    Update client.ts

--- Lab5/client.ts
+++ Lab5/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const REMOTE_SERVER = "http://localhost:10000";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const fetchWelcomeMessage = async () => {
   const response = await axios.get(`${REMOTE_SERVER}/lab5/welcome`);
   return response.data;
