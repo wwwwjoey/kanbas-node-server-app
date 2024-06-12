142cfb2f5742e33f66decc49f61277c98bc2412c
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-12 00:47:21 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
-import CourseRoutes from "./Kanbas/Courses/routes.js";
-import ModuleRoutes from "./Lab5/Modules/routes.js";
+
 
 
 
