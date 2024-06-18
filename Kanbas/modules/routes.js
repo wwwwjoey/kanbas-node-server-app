e92c571426ba91109a63ea45ce6c90f88f7355c3
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-18 04:53:16 -0400

    Update routes.js

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,44 +1,38 @@
-import express from 'express';
-import Hello from "./Hello.js"
-import Lab5 from "./Lab5/index.js";
-import cors from "cors";
-import AssignmentRoutes from './Kanbas/assignments/routes.js';
-import mongoose from "mongoose";
-import "dotenv/config";
-import session from "express-session";
-import "dotenv/config";
-import CourseRoutes from "./Kanbas/Courses/routes.js";
-import ModuleRoutes from  "./Kanbas/modules/routes.js"
+import db from "../Database/index.js";
+export default function ModuleRoutes(app) {
+    app.get("/api/courses/:cid/modules", (req, res) => {
+        const { cid } = req.params;
+        const modules = db.modules.filter((m) => m.course === cid);
+        res.json(modules);
+    });
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
-mongoose.connect(CONNECTION_STRING);
-const app = express();
-app.use(cors({   credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",})); 
-    
+    app.post("/api/courses/:cid/modules", (req, res) => {
+        const { cid } = req.params;
+        const newModule = {
+            ...req.body,
+            course: cid,
+            _id: new Date().getTime().toString(),
+        };
+        db.modules.push(newModule);
+        res.send(newModule);
+    });
 
-app.use(express.json());
-const sessionOptions = {
-    secret: process.env.SESSION_SECRET || "kanbas",
-    resave: false,
-    saveUninitialized: false,
-  };
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.NODE_SERVER_DOMAIN,
-    };
-  }
-  app.use(session(sessionOptions));
-  
-  
+    app.delete("/api/modules/:mid", (req, res) => {
+        const { mid } = req.params;
+        db.modules = db.modules.filter((m) => m._id !== mid);
+        res.sendStatus(200);
+    });
 
-AssignmentRoutes(app);
-CourseRoutes(app);
-ModuleRoutes(app);
-Hello(app);
-Lab5(app);
+    app.put("/api/modules/:mid", (req, res) => {
+        const { mid } = req.params;
+        const moduleIndex = db.modules.findIndex(
+            (m) => m._id === mid);
+        db.modules[moduleIndex] = {
+            ...db.modules[moduleIndex],
+            ...req.body
+        };
+        res.sendStatus(204);
+    });
 
-app.listen(process.env.PORT || 4000);
+
+}
