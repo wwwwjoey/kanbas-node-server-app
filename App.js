dfb6c2977f0bf462e5d4062dfad6ed770d7110f2
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-18 04:02:17 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
-import CourseRoutes from "./Users/routes.js";
-import ModuleRoutes from "./Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js"
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
 import mongoose from "mongoose";
 import "dotenv/config";
-import UserRoutes from "./Users/routes.js";
+import UserRoutes from './Users/routes.js';
 import session from "express-session";
 import "dotenv/config";
 
