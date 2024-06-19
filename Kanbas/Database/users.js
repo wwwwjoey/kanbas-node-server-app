5ca9e8561c1b9125c781fddb92e4dc06de0f0742
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-19 16:11:03 -0400

    Update users.js

--- Kanbas/Database/users.js
+++ Kanbas/Database/users.js
@@ -1,144 +1,92 @@
-export default [{
-  "_id": {
-    "$oid": "667333ff3ddfccdea0d0006b"
-  },
-  "username": "iron_man",
-  "password": "stark123",
-  "firstName": "Tony",
-  "lastName": "Stark",
-  "email": "tony@stark.com",
-  "dob": "1970-05-29",
-  "role": "STUDENT",
-  "loginId": "001234561S",
-  "section": "S101",
-  "lastActivity": "2020-10-01",
-  "totalActivity": "10:21:32"
-},
-{
-  "_id": {
-    "$oid": "667333ff3ddfccdea0d0006c"
-  },
-  "username": "dark_knight",
-  "password": "wayne123",
-  "firstName": "Bruce",
-  "lastName": "Wayne",
-  "email": "bruce@wayne.com",
-  "dob": "1972-02-19",
-  "role": "STUDENT",
-  "loginId": "001234562S",
-  "section": "S101",
-  "lastActivity": "2020-11-02",
-  "totalActivity": "15:32:43"
-},
-{
-  "_id": {
-    "$oid": "667333ff3ddfccdea0d0006d"
-  },
-  "username": "capt_america",
-  "password": "rogers123",
-  "firstName": "Steve",
-  "lastName": "Rogers",
-  "email": "steve@avengers.com",
-  "dob": "1918-07-04",
-  "role": "STUDENT",
-  "loginId": "001234563S",
-  "section": "S101",
-  "lastActivity": "2020-10-02",
-  "totalActivity": "23:32:43"
-},
-{
-  "_id": {
-    "$oid": "667333ff3ddfccdea0d0006e"
-  },
-  "username": "black_widow",
-  "password": "romanoff123",
-  "firstName": "Natasha",
-  "lastName": "Romanoff",
-  "email": "natasha@avengers.com",
-  "dob": "1984-11-22",
-  "role": "TA",
-  "loginId": "001234564S",
-  "section": "S101",
-  "lastActivity": "2020-11-05",
-  "totalActivity": "13:23:34"
-},
-{
-  "_id": {
-    "$oid": "667333ff3ddfccdea0d0006f"
-  },
-  "username": "thor_odinson",
-  "password": "mjolnir123",
-  "firstName": "Thor",
-  "lastName": "Odinson",
-  "email": "thor@asgard.com",
-  "dob": "982-05-25",
-  "role": "STUDENT",
-  "loginId": "001234565S",
-  "section": "S101",
-  "lastActivity": "2020-12-01",
-  "totalActivity": "11:22:33"
-},
-{
-  "_id": {
-    "$oid": "667333ff3ddfccdea0d00070"
-  },
-  "username": "hulk_smash",
-  "password": "banner123",
-  "firstName": "Bruce",
-  "lastName": "Banner",
-  "email": "bruce@avengers.com",
-  "dob": "1969-12-18",
-  "role": "STUDENT",
-  "loginId": "001234566S",
-  "section": "S101",
-  "lastActivity": "2020-12-01",
-  "totalActivity": "22:33:44"
-},
-{
-  "_id": {
-    "$oid": "667333ff3ddfccdea0d00071"
-  },
-  "username": "ring_bearer",
-  "password": "shire123",
-  "firstName": "Frodo",
-  "lastName": "Baggins",
-  "email": "frodo@shire.com",
-  "dob": "1368-09-22",
-  "role": "FACULTY",
-  "loginId": "001234567S",
-  "section": "S101",
-  "lastActivity": "2020-12-02",
-  "totalActivity": "44:33:22"
-},
-{
-  "_id": {
-    "$oid": "667333ff3ddfccdea0d00072"
-  },
-  "username": "strider",
-  "password": "aragorn123",
-  "firstName": "Aragorn",
-  "lastName": "Elessar",
-  "email": "aragorn@gondor.com",
-  "dob": "2931-03-01",
-  "role": "TA",
-  "loginId": "001234568S",
-  "section": "S101",
-  "lastActivity": "2020-12-04",
-  "totalActivity": "12:23:34"
-},
-{
-  "_id": {
-    "$oid": "667333ff3ddfccdea0d00073"
-  },
-  "username": "elf_archer",
-  "password": "legolas123",
-  "firstName": "Legolas",
-  "lastName": "Greenleaf",
-  "email": "legolas@mirkwood.com",
-  "dob": "2879-07-15",
-  "role": "FACULTY",
-  "loginId": "001234569S",
-  "section": "S101",
-  "lastActivity": "2020-11-11",
-  "totalActivity": "21:32:43"
-}]
+export default [
+    {
+      "_id": "121",
+      "username": "iron_man",
+      "password": "stark123",
+      "firstName": "Tony",
+      "lastName": "Stark",
+      "email": "tony@stark.com",
+      "dob": "1970-05-29",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "122",
+      "username": "dark_knight",
+      "password": "wayne123",
+      "firstName": "Bruce",
+      "lastName": "Wayne",
+      "email": "bruce@wayne.com",
+      "dob": "1972-02-19",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "123",
+      "username": "capt_america",
+      "password": "rogers123",
+      "firstName": "Steve",
+      "lastName": "Rogers",
+      "email": "steve@avengers.com",
+      "dob": "1918-07-04",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "124",
+      "username": "black_widow",
+      "password": "romanoff123",
+      "firstName": "Natasha",
+      "lastName": "Romanoff",
+      "email": "natasha@avengers.com",
+      "dob": "1984-11-22",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "125",
+      "username": "thor_odinson",
+      "password": "mjolnir123",
+      "firstName": "Thor",
+      "lastName": "Odinson",
+      "email": "thor@asgard.com",
+      "dob": "982-05-25",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "126",
+      "username": "hulk_smash",
+      "password": "banner123",
+      "firstName": "Bruce",
+      "lastName": "Banner",
+      "email": "bruce@avengers.com",
+      "dob": "1969-12-18",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "127",
+      "username": "ring_bearer",
+      "password": "shire123",
+      "firstName": "Frodo",
+      "lastName": "Baggins",
+      "email": "frodo@shire.com",
+      "dob": "1368-09-22",
+      "role": "FACULTY"
+    },
+    {
+      "_id": "128",
+      "username": "strider",
+      "password": "aragorn123",
+      "firstName": "Aragorn",
+      "lastName": "Elessar",
+      "email": "aragorn@gondor.com",
+      "dob": "2931-03-01",
+      "role": "FACULTY"
+    },
+    {
+      "_id": "129",
+      "username": "elf_archer",
+      "password": "legolas123",
+      "firstName": "Legolas",
+      "lastName": "Greenleaf",
+      "email": "legolas@mirkwood.com",
+      "dob": "2879-07-15",
+      "role": "FACULTY"
+    }
+  ]
