1c96ddda2181885ad2bca6f63ed401d4888ba407
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-22 16:19:50 -0400

    Update routes.js

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,32 +1,43 @@
 import * as dao from "./dao.js";
 
 export default function CourseRoutes(app) {
-  app.get("/api/courses", (req, res) => {
-    const courses = Database.courses;
-    res.send(courses);
-  });
+    app.put("/api/courses/:id", async (req, res) => {
+        const { id } = req.params;
+        const course = req.body;
+        try {
+            await dao.updateCourse(id, course);
+            res.sendStatus(204);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
 
-  app.post("/api/courses", (req, res) => {
-    const course = { ...req.body,
-      _id: new Date().getTime().toString() };
-    Database.courses.push(course);
-    res.send(course);
-  });
+    app.delete("/api/courses/:id", async (req, res) => {
+        const { id } = req.params;
+        try {
+            await dao.deleteCourse(id);
+            res.sendStatus(204);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
 
-  app.delete("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    Database.courses = Database.courses.filter((c) => c._id !== id);
-    res.sendStatus(204);
-  });
+    app.post("/api/courses", async (req, res) => {
+        const course = req.body;
+        try {
+            const newCourse = await dao.createCourse(course);
+            res.json(newCourse);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
 
-  app.put("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    const course = req.body;
-    Database.courses = Database.courses.map((c) =>
-      c._id === id ? { ...c, ...course } : c
-    );
-    res.sendStatus(204);
-  });
-
-
+    app.get("/api/courses", async (req, res) => {
+        try {
+            const courses = await dao.findAllCourses();
+            res.json(courses);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
 }
