6cd9fb9c011388c669fb30ad6f0cfd0b71fbcb4a
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-22 16:12:49 -0400

    Create dao.js

---
+++ Kanbas/modules/dao.js
@@ -0,0 +1,15 @@
+import model from "./model.js";
+
+export const createModule = (module) => {
+    delete module._id;
+    return model.create(module);
+};
+
+export const findAllModulesForCourse = (courseId) =>
+    model.find({ course: courseId });
+
+export const updateModule = (moduleId, module) =>
+    model.updateOne({ _id: moduleId }, { $set: module });
+
+export const deleteModule = (moduleId) =>
+    model.deleteOne({ _id: moduleId });
