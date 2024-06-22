1650610b11ebf4d4d42a69869db2785c47180dc4
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-22 16:24:30 -0400

    Update routes.js

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,38 +1,48 @@
-import db from "../Database/index.js";
+import * as dao from "./dao.js";
+
 export default function ModuleRoutes(app) {
-    app.get("/api/courses/:cid/modules", (req, res) => {
-        const { cid } = req.params;
-        const modules = db.modules.filter((m) => m.course === cid);
-        res.json(modules);
+    app.put("/api/modules/:mid", async (req, res) => {
+        const { mid } = req.params;
+        const module = req.body;
+        try {
+            await dao.updateModule(mid, module);
+            res.sendStatus(204);
+        } catch (error) {
+            res.status(500).send(error);
+        }
     });
 
-    app.post("/api/courses/:cid/modules", (req, res) => {
+    app.delete("/api/modules/:mid", async (req, res) => {
+        const { mid } = req.params;
+        try {
+            await dao.deleteModule(mid);
+            res.sendStatus(200);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+
+    app.post("/api/courses/:cid/modules", async (req, res) => {
         const { cid } = req.params;
         const newModule = {
             ...req.body,
             course: cid,
-            _id: new Date().getTime().toString(),
         };
-        db.modules.push(newModule);
-        res.send(newModule);
+        try {
+            const createdModule = await dao.createModule(newModule);
+            res.json(createdModule);
+        } catch (error) {
+            res.status(500).send(error);
+        }
     });
 
-    app.delete("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        db.modules = db.modules.filter((m) => m._id !== mid);
-        res.sendStatus(200);
+    app.get("/api/courses/:cid/modules", async (req, res) => {
+        const { cid } = req.params;
+        try {
+            const modules = await dao.findAllModulesForCourse(cid);
+            res.json(modules);
+        } catch (error) {
+            res.status(500).send(error);
+        }
     });
-
-    app.put("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        const moduleIndex = db.modules.findIndex(
-            (m) => m._id === mid);
-        db.modules[moduleIndex] = {
-            ...db.modules[moduleIndex],
-            ...req.body
-        };
-        res.sendStatus(204);
-    });
-
-
 }
