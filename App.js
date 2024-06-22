ca6485b870597e14341c5ebbfdfb1d553180f045
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-22 16:48:17 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,51 @@
-
-import express from 'express';
-import Hello from "./Hello.js"
-import Lab5 from "./Lab5/index.js";
-import cors from "cors";
-import CourseRoutes from './Kanbas/Courses/routes.js';
-import ModuleRoutes from "./Kanbas/modules/routes.js";
-import AssignmentRoutes from './Kanbas/assignments/routes.js';
-import mongoose from "mongoose";
 import "dotenv/config";
-import UserRoutes from './Kanbas/Users/routes.js';
+import express from 'express';
+import Hello from "./Hello.js";
+import Lab5 from './Lab5/index.js';
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+import UserRoutes from "./Kanbas/Users/routes.js"
+import cors from "cors";
+import mongoose from 'mongoose';
+import session from "express-session";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
 
-const app = express();
-app.use(cors()); 
+const app = express()
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    }));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(
+    session(sessionOptions));
+
 app.use(express.json());
 
-AssignmentRoutes(app);
-CourseRoutes(app);
+UserRoutes(app);
 ModuleRoutes(app);
+CourseRoutes(app);
+AssignmentRoutes(app);
+
 Hello(app);
 Lab5(app);
-UserRoutes(app);
 
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4000)
