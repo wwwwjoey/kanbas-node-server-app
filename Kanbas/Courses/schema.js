5e443de1568f58ce9dd7e9bac4a112a15d35c19b
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-22 16:09:22 -0400

    Update schema.js

--- Kanbas/Courses/schema.js
+++ Kanbas/Courses/schema.js
@@ -1,1 +1,14 @@
-
+import mongoose from "mongoose";
+const courseSchema = new mongoose.Schema({
+    number: { type: String, required: true, unique: true },
+    name: String,
+    startDate: Date,
+    endDate: Date,
+    department: String,
+    credits: Number,
+    description: String,
+    image: String
+},
+{ collection: "courses" }
+);
+export default courseSchema;
