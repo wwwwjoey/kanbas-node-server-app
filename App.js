9b11da816fc0426c86fae095c4e2a8387338b407
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-22 21:19:53 -0400

    Update App.js

--- App.js
+++ App.js
@@ -29,7 +29,6 @@ if (process.env.NODE_ENV !== "development") {
     sessionOptions.cookie = {
         sameSite: "none",
         secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
 app.use(
