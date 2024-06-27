0c61c4ed173757cb5da1cd67cfec9ec9d31b9a67
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-27 15:03:37 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,52 +1,50 @@
 import "dotenv/config";
-import express from 'express';
+import express from "express";
 import Hello from "./Hello.js";
-import Lab5 from './Lab5/index.js';
-import CourseRoutes from "./Kanbas/Courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-import UserRoutes from "./Kanbas/Users/routes.js"
+import Lab5 from "./Lab5/index.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
-import mongoose from 'mongoose';
-import session from "express-session";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import UserRoutes from "./Users/routes.js";
+import mongoose from "mongoose";
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
 
-const app = express()
+import session from "express-session";
+import QuizRoutes from "./Kanbas/quizzes/routes.js";
+
+const app = express();
 app.use(
-    cors({
-        credentials: true,
-        origin: process.env.NETLIFY_URL || "http://localhost:3000",
-    }));
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
+
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET || "kanbas",
-    resave: false,
-    saveUninitialized: false,
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
 };
 if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
-    };
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
 }
-app.use(
-    session(sessionOptions));
+
+app.use(session(sessionOptions));
 
 app.use(express.json());
-
-
-UserRoutes(app);
-ModuleRoutes(app);
-CourseRoutes(app);
-AssignmentRoutes(app);
-
 Hello(app);
 Lab5(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+UserRoutes(app);
+QuizRoutes(app);
 
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 4000);
--- Hello.js
+++ Hello.js
@@ -1,9 +1,12 @@
 export default function Hello(app) {
+    app.get("/lab5/welcome", (req, res) => {
+        res.send("Welcome to Lab 5");
+    });
+
     app.get('/hello', (req, res) => {
-      res.send('Life is good!')
-    })
+        res.send('Life is good!')
+    });
     app.get('/', (req, res) => {
-      res.send('Welcome to Full Stack Development!')
-    })
-  }
-  
+        res.send('Welcome to Full Stack Development!')
+    });
+}
